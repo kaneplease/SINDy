34f655c9134357d6294cee5541fddd3796a48798
Atsuki <piacere.atsuki@gmail.com>
2019-11-06 17:41:23 +0900

    Boltzmann_sparce_normalizeの応用バージョンを追加

--- utils/CreateTheta.py
+++ utils/CreateTheta.py
@@ -54,6 +54,61 @@ def CreateTheta(x, order):
 
     return Theta
 
+#   1.0を含まないように重回帰分析
+def CreateTheta_noConst(x, order):
+    # x = [[x[0] x[1] ... x[n]]
+    #      [y[0] y[1] ... y[n]]
+    #      [z[0] z[1] ... z[n]]]
+    # numpy array
+
+    n = x[0].size
+    xdim = len(x)
+
+    # Theta = np.array([])
+
+    # order 0
+    tmp = [[1.0 for j in range(n)]]
+    Theta = tmp
+
+    # order 1
+    for i in range(xdim):
+        Theta = np.concatenate((Theta, [[x[i][j] for j in range(n)]]), axis=0)
+
+    # order 2
+    if order >= 2:
+        for i in range(xdim):
+            for j in range(i+1):    #　　木構造により場合の数を尽くす（ノート参照）
+                Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] for a in range(n)]]), axis=0)
+
+    # order 3
+    if order >= 3:
+        for i in range(xdim):
+            for j in range(i+1):
+                for k in range(j+1):
+                    Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] for a in range(n)]]), axis=0)
+
+    # order 4
+    if order >= 4:
+        for i in range(xdim):
+            for j in range(i+1):
+                for k in range(j+1):
+                    for l in range(k+1):
+                        Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] for a in range(n)]]),
+                                               axis=0)
+
+    # order 5
+    if order >= 5:
+        for i in range(xdim):
+            for j in range(i+1):
+                for k in range(j+1):
+                    for l in range(k+1):
+                        for m in range(l+1):
+                            Theta = np.concatenate(
+                                (Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] * x[m][a] for a in range(n)]]), axis=0)
+    # 0行目を削除
+    Theta = np.delete(Theta, 0, 0)
+    return Theta
+
 #   全ての係数をmu = 0, std = 1で正規化（時間軸に沿って）
 def CreateTheta_normal(x, order):
     Theta = CreateTheta(x, order)
