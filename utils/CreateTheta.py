af5b93e2cbdb91d87749fb9c8c3dfda0daa5e632
Atsuki <piacere.atsuki@gmail.com>
2020-01-07 22:37:22 +0900

    データの読み込みを自動化

--- utils/CreateTheta.py
+++ utils/CreateTheta.py
@@ -1,6 +1,59 @@
 import numpy as np
 
 # まだsinを入れていない、入れる必要はあるか？
+# def CreateTheta(x, order):
+#     # x = [[x[0] x[1] ... x[n]]
+#     #      [y[0] y[1] ... y[n]]
+#     #      [z[0] z[1] ... z[n]]]
+#     # numpy array
+#
+#     n = x[0].size
+#     xdim = len(x)
+#
+#     # Theta = np.array([])
+#
+#     # order 0
+#     tmp = [[1.0 for j in range(n)]]
+#     Theta = tmp
+#
+#     # order 1
+#     for i in range(xdim):
+#         Theta = np.concatenate((Theta, [[x[i][j] for j in range(n)]]), axis=0)
+#
+#     # order 2
+#     if order >= 2:
+#         for i in range(xdim):
+#             for j in range(i+1):    #　　木構造により場合の数を尽くす（ノート参照）
+#                 Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] for a in range(n)]]), axis=0)
+#
+#     # order 3
+#     if order >= 3:
+#         for i in range(xdim):
+#             for j in range(i+1):
+#                 for k in range(j+1):
+#                     Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] for a in range(n)]]), axis=0)
+#
+#     # order 4
+#     if order >= 4:
+#         for i in range(xdim):
+#             for j in range(i+1):
+#                 for k in range(j+1):
+#                     for l in range(k+1):
+#                         Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] for a in range(n)]]),
+#                                                axis=0)
+#
+#     # order 5
+#     if order >= 5:
+#         for i in range(xdim):
+#             for j in range(i+1):
+#                 for k in range(j+1):
+#                     for l in range(k+1):
+#                         for m in range(l+1):
+#                             Theta = np.concatenate(
+#                                 (Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] * x[m][a] for a in range(n)]]), axis=0)
+#
+#     return Theta
+
 def CreateTheta(x, order):
     # x = [[x[0] x[1] ... x[n]]
     #      [y[0] y[1] ... y[n]]
@@ -13,25 +66,25 @@ def CreateTheta(x, order):
     # Theta = np.array([])
 
     # order 0
-    tmp = [[1.0 for j in range(n)]] 
+    tmp = [[1.0 for j in range(n)]]
     Theta = tmp
 
     # order 1
     for i in range(xdim):
-        Theta = np.concatenate((Theta, [[x[i][j] for j in range(n)]]), axis=0)
+        Theta = np.concatenate((Theta, [x[i]]), axis=0)
 
     # order 2
     if order >= 2:
         for i in range(xdim):
             for j in range(i+1):    #　　木構造により場合の数を尽くす（ノート参照）
-                Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] for a in range(n)]]), axis=0)
+                Theta = np.concatenate((Theta, [x[i] * x[j]]), axis=0)
 
     # order 3
     if order >= 3:
         for i in range(xdim):
             for j in range(i+1):
                 for k in range(j+1):
-                    Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] for a in range(n)]]), axis=0)
+                    Theta = np.concatenate((Theta, [x[i] * x[j] * x[k]]), axis=0)
 
     # order 4
     if order >= 4:
@@ -39,7 +92,7 @@ def CreateTheta(x, order):
             for j in range(i+1):
                 for k in range(j+1):
                     for l in range(k+1):
-                        Theta = np.concatenate((Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] for a in range(n)]]),
+                        Theta = np.concatenate((Theta, [x[i] * x[j] * x[k] * x[l]]),
                                                axis=0)
 
     # order 5
@@ -50,7 +103,7 @@ def CreateTheta(x, order):
                     for l in range(k+1):
                         for m in range(l+1):
                             Theta = np.concatenate(
-                                (Theta, [[x[i][a] * x[j][a] * x[k][a] * x[l][a] * x[m][a] for a in range(n)]]), axis=0)
+                                (Theta, [x[i] * x[j] * x[k] * x[l] * x[m]]), axis=0)
 
     return Theta
 
